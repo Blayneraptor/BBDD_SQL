9f973109d1742f861bc72ef8a9e6bfceff9eef31
Blayneraptor <cousoworks@gmail.com>
2024-04-26 01:41:37 +0200

    Añadiendo solución ejercicio 1

--- MULTITABLAS/MULTI_EMPLEADOS/MULTI_EMPLEADOS.sql
+++ MULTITABLAS/MULTI_EMPLEADOS/MULTI_EMPLEADOS.sql
@@ -1,6 +1,8 @@
 -- 1.  Mostrar para cada empleado, el nombre de departamento y el nombre de dicho empleado y su salario.
 
-
+SELECT e.EMP_NO, e.APELLIDO, d.DNOMBRE AS NOMBRE_DEPARTAMENTO, e.SALARIO
+FROM empleados e
+INNER JOIN departamentos d  ON e.DEPT_NO = d.DEPT_NO;
 
 -- 2. Mostrar el nombre de cada empleado y la localidad del departamento en el que trabaja.
 

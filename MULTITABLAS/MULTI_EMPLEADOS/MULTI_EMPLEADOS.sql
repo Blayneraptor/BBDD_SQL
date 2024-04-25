71af030bbd5dad5de2e5648b0cd30045a8e4f471
Blayneraptor <cousoworks@gmail.com>
2024-04-26 01:24:57 +0200

    Añadiendo multitablas

---
+++ MULTITABLAS/MULTI_EMPLEADOS/MULTI_EMPLEADOS.sql
@@ -0,0 +1,18 @@
+-- 1.  Mostrar para cada empleado, el nombre de departamento y el nombre de dicho empleado y su salario.
+
+
+
+-- 2. Mostrar el nombre de cada empleado y la localidad del departamento en el que trabaja.
+
+
+
+-- 3. Mostrar el nombre del departamento y la media de las comisiones de sus empleados.
+
+
+
+-- 4. Mostrar los nombres de los departamentos y el número de empleados de los departamentos que tienen más de dos empleados.
+
+
+
+-- 5. Mostrar cuántos empleados hay en cada ciudad de los departamentos.
+

9742971f1df068bea3fe8a7f67009c4ae3953ccb
Blayneraptor <cousoworks@gmail.com>
2024-04-28 17:35:19 +0200

    Añadiendo ejercicio 5 solución

--- SUBCONSULTAS/SUBCONSULTA_TIENDA2/SUBCONSULTAS_TIENDA2.sql
+++ SUBCONSULTAS/SUBCONSULTA_TIENDA2/SUBCONSULTAS_TIENDA2.sql
@@ -2,19 +2,32 @@
 
 SELECT nombre,max(precio)
 FROM producto 
-WHERE id_fabricante IN (SELECT nombre FROM fabricante WHERE nombre LIKE "Lenovo");
+WHERE id_fabricante IN 
+    (SELECT nombre 
+    FROM fabricante 
+    WHERE nombre 
+    LIKE "Lenovo");
 
 -- 2. Nombre del producto más caro de Lenovo.
 
 SELECT *
 FROM producto
-WHERE precio = (SELECT MAX(precio) FROM producto WHERE id_fabricante = (SELECT id FROM fabricante WHERE nombre = 'Lenovo'));
+WHERE precio = 
+    (SELECT MAX(precio) 
+    FROM producto 
+    WHERE id_fabricante = 
+    (SELECT id 
+    FROM fabricante 
+    WHERE nombre = 'Lenovo'));
 
 -- 3. Nombre del producto más barato de Hewlett-Packard.
 
 SELECT nombre,min(precio)
 FROM producto
-WHERE id_fabricante IN (SELECT id FROM fabricante WHERE nombre = "Hewlett-Packard");
+WHERE id_fabricante IN 
+    (SELECT id 
+    FROM fabricante 
+    WHERE nombre = "Hewlett-Packard");
 
 -- 4. Productos con precio mayor o igual al producto más caro de Lenovo.
 
@@ -31,6 +44,19 @@ WHERE precio >= (
 );
 
 -- 5. Productos de Asus con precio superior al precio medio de todos sus productos.
+
+SELECT p.*
+FROM producto p
+WHERE p.id_fabricante = (
+    SELECT id
+    FROM fabricante
+    WHERE nombre = 'Asus')
+        AND p.precio > (
+        SELECT AVG(precio) 
+        FROM producto 
+        WHERE id_fabricante = p.id_fabricante);
+
+
 -- 6. Fabricantes con el mismo número de productos que Lenovo (usando subconsultas en la cláusula HAVING).
 -- 7. Fabricantes con productos asociados (utilizando ALL o ANY).
 -- 8. Fabricantes sin productos asociados (utilizando IN o NOT IN).
